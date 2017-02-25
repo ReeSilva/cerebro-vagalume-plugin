33fdb70f9d83c1b1288cb54a581efc2654432803
Renato Biancalana da Silva <renato.biancalana.silva@gmail.com>
2017-02-25 03:11:12 -0300

    Primeiro desenvolvimento, apenas para treinar

--- src/index.js
+++ src/index.js
@@ -1,14 +1,52 @@
 'use strict';
+const Vagalume = require('vagalume');
+const vagalumeApi = new Vagalume();
+const co = require('co');
 
-const plugin = ({term, display, actions}) => {
+const vagalumeApiKey = '387980fe1f207402d2b0cd1bba3267cf';
+
+const plugin = co.wrap(function* plugin ({term, display, actions, hide}) {
   // It is your main plugin function
   // do something and call display() with your results
-  display({
-    title: 'It works',
-    subtitle: `You entered ${term}`
-  });
-};
+  const match = term.match(/^vagalume\s(.+)/);
+  if (match) {
+    let musicas = '';
+    
+    display({
+      id: 'vagalume',
+      title: `Procurando por ${match[1]} no Vagalume...`
+    })
+    
+    try{
+      musicas = yield vagalumeApi.getByTrecho(match[1], 5);
+    }
+    catch (e) {
+      throw e;
+    }
+    
+    hide('vagalume');
+    
+    musicas.response.docs.map(co.wrap(function* (doc) {
+      const response = yield fetch(`https://api.vagalume.com.br/search.php?musid=${doc.id}&apikey=${vagalumeApiKey}`);
+      const finalResponse = yield response.json();
+      
+      display({
+        title: `${doc.band} - ${doc.title}`,
+        subtitle: doc.url,
+        getPreview: () => (
+          `<div>
+            <h2>${doc.band} - ${doc.title}</h2>
+            <div>${finalResponse.mus[0].text.substring(0, 250)}</div>
+          </div>`
+        ),
+        onSelect: (event) => actions.open(`https://www.vagalume.com.br${doc.url}`)
+      });
+    }));
+  };
+});
 
 module.exports = {
+  name: 'Procure músicas no Vagalume',
+  keyword: 'vagalume',
   fn: plugin
 }
