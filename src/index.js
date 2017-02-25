bd7c41b7eec6d0cf5bda8c143b899ab49ab1c469
Renato Biancalana da Silva <renato.biancalana.silva@gmail.com>
2017-02-25 00:44:44 -0300

    first commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@
 const plugin = ({term, display, actions}) => {
   // It is your main plugin function
   // do something and call display() with your results
+  display({
+    title: 'It works',
+    subtitle: `You entered ${term}`
+  });
 };
 
 module.exports = {
